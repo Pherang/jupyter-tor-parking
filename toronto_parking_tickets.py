ab6f6a4583ba861204f52bff90169b8509393fc2
Pherang <pherang@users.noreply.github.com>
2018-09-30 14:34:27 -0400

    Init Repo

---
+++ toronto_parking_tickets.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# # What Parking Tickets Mean for Toronto
+# 
+# Parking violations and parking tickets are a contentious topic for Torontonians.
+# 
+# Toronto parking regulations have a reputation for being confusing and misleading. In this notebook we study parking tickets and to see what impact they have on the city and its citizens.
+# 
+# The data was obtained form the city of Toronto's Open Data catalogue and can be downloaded [here](https://www.toronto.ca/city-government/data-research-maps/open-data/open-data-catalogue/#75d14c24-3b7e-f344-4412-d8fd41f89455)
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
